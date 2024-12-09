480941c50f9388038c091cede71cd49fe53af96e
ml4economics <karsten.opitz@studium.fernuni-hagen.de>
2024-12-09 22:12:06 +0100

    qq plots

--- rstudio/pp_plot_vs_qq_plot.R
+++ rstudio/pp_plot_vs_qq_plot.R
@@ -1,8 +1,11 @@
-x <- seq(-10, 10, length.out = 100)
+library(ggplot2)
+library(patchwork)
+
 mu <- 0  
 sd_values <- c(0.5, 1, 2) 
 
-# Create a data frame with densities
+# Dichtefunktionen
+x <- seq(-6, 6, length.out = 100)
 plot_data <- data.frame(
   x = rep(x, times = length(sd_values)),
   y = c(dnorm(x, mean = mu, sd = sd_values[1]),
@@ -10,30 +13,41 @@ plot_data <- data.frame(
         dnorm(x, mean = mu, sd = sd_values[3])),
   sd = factor(rep(sd_values, each = length(x)))
 )
-ggplot(plot_data, aes(x = x, y = y, color = sd)) +
+p1 <- ggplot(plot_data, aes(x = x, y = y, color = sd)) +
   geom_line(size = 1) +
-  ggtitle("Normalverteilung mit verschiedenen Standardabweichungen") +
+  ggtitle("Normalverteilungen") +
   xlab("Wert") +
   ylab("Dichte") +
   theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5)
+  ) +  
   scale_color_discrete(name = "Standardabweichung")
 
+# PP Plot
+x <- seq(-10, 10, length.out = 100)
+ref = pnorm(x, mean = mu, sd = sd_values[2])
 plot_data <- data.frame(
-  x = rep(x, times = length(sd_values)),
-  y = c(dnorm(x, mean = mu, sd = sd_values[1]),
-        dnorm(x, mean = mu, sd = sd_values[2]),
-        dnorm(x, mean = mu, sd = sd_values[3])),
+  x = rep(ref, times = length(sd_values)),
+  y = c(pnorm(x, mean = mu, sd = sd_values[1]),
+        ref,
+        pnorm(x, mean = mu, sd = sd_values[3])),
   sd = factor(rep(sd_values, each = length(x)))
 )
-ggplot(plot_data, aes(x = x, y = y, color = sd)) +
-  geom_line(size = 1) +
-  ggtitle("Normal Distributions with Different Standard Deviations") +
-  xlab("Value") +
-  ylab("Density") +
+p2 <- ggplot(plot_data, aes(x = x, y = y, color = sd)) +
+  geom_line(linewidth = 1) +
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "red") +
+  ggtitle("PP Plot") +
+  xlab("Wert 1") +
+  ylab("Wert 2") +
   theme_minimal() +
-  scale_color_discrete(name = "Standard Deviation")
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    legend.position = "none"
+  ) +
+  scale_color_discrete(name = "Standardabweichung")
 
-
+# QQ Plot
 x <- seq(0, 1, length.out = 100)
 ref = qnorm(x, mean = mu, sd = sd_values[2])
 plot_data <- data.frame(
@@ -44,10 +58,17 @@ plot_data <- data.frame(
   sd = factor(rep(sd_values, each = length(x)))
 )
 
-ggplot(plot_data, aes(x = x, y = y, color = sd)) +
+p3 <- ggplot(plot_data, aes(x = x, y = y, color = sd)) +
   geom_point() +
   geom_abline(intercept = 0, slope = 1, linetype = "dashed", color = "red") +
-  ggtitle("Empirical CDF vs. Theoretical CDF") +
-  xlab("Theoretical CDF") +
-  ylab("Empirical CDF") +
-  theme_minimal()
+  ggtitle("QQ Plot") +
+  xlab("Standardnormalverteilung") +
+  ylab("Skalierte Normalverteilung") +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5),
+    legend.position = "none"
+  ) +
+  scale_color_discrete(name = "Standardabweichung")
+
+p1 / (p2 + p3)
