42efc63d2e163b2ae1df0d0240329fee30fc13b3
ml4economics <karsten.opitz@studium.fernuni-hagen.de>
2024-12-22 17:14:21 +0100

    illustrate qq_plot

--- jupyter/util.R
+++ jupyter/util.R
@@ -8,6 +8,15 @@ partial_func <- function(func, ...) {
   }
 }
 
+simple_ggplot <- function(data, xlab, ylab, color, title) {
+  ggplot(data, 
+         aes(x = x, y = y)) + 
+         geom_line(color = color) +
+         xlab(xlab) + 
+         ylab(ylab) + 
+         ggtitle(title)
+}
+
 empty_plot <- function() {
   ggplot() + theme_void() + theme(aspect.ratio = 1)
 }
@@ -21,7 +30,7 @@ jupyter_theme <- function() {
   theme(aspect.ratio = 1,
         axis.title = element_text(size = 15, color = "black"),        
         axis.text = element_text(size = 12, color = "black"),
-        legend.title = element_text(size = 15, color = "black"),
-        legend.text = element_text(size = 12, color = "black"),
+        legend.title = element_text(size = 20, color = "black"),
+        legend.text = element_text(size = 17, color = "black"),
         plot.title = element_text(size=20, color = "black", hjust = 0.5))
 }
--- rstudio/qqplot_illustrate.R
+++ rstudio/qqplot_illustrate.R
@@ -1,22 +1,31 @@
 source("../jupyter/util.R")
 library(patchwork)
 
-create_plot <- function(data, xlab, ylab, color, title) {
-  ggplot(data, 
-         aes(x = x, y = y)) + 
-         geom_line(color = color) +
-         xlab(xlab) + 
-         ylab(ylab) + 
-         ggtitle(title) +
-         rstudio_theme()
+densities_plot <- function(samples, 
+                           distribution1, dfunc1, 
+                           distribution2, dfunc2, 
+                           color1, color2, title) {
+  data <- data.frame(x = samples, 
+                     density1 = dfunc1(samples), 
+                     density2 = dfunc2(samples))
+  gg <- ggplot(data) +
+    geom_line(aes(x = samples, y = density1, color = "color1")) +
+    geom_line(aes(x = samples, y = density2, color = "color2")) +
+    xlab("x") + ylab("p") +
+    ggtitle(title) +
+    scale_color_manual(name = NULL, 
+                       values = c("color1" = color1, "color2" = color2), 
+                       labels = c(distribution1, distribution2) ) +
+    theme(legend.background = element_rect(fill = "white", color = "black"))
 }
 
 illustrate_qqplot <- function(x_low, x_high,
-                              ref_distribution, ref_dfunc, ref_qfunc,
-                              test_distribution, test_dfunc, test_qfunc) {
+                              ref_distribution, ref_dfunc, ref_pfunc, ref_qfunc,
+                              test_distribution, test_dfunc, test_pfunc, test_qfunc,
+                              theme) {
   samples <- seq(x_low, x_high, length.out = 100)
-  ref_prob  <- ref_dfunc(q = samples)
-  test_prob <- test_dfunc(q = samples)
+  ref_prob  <- ref_pfunc(q = samples)
+  test_prob <- test_pfunc(q = samples)
   
   ref_data  <- data.frame(x = samples,   y = ref_prob)
   test_data <- data.frame(x = test_prob, y = samples)
@@ -27,37 +36,37 @@ illustrate_qqplot <- function(x_low, x_high,
   below_x_high <- which(test_quantiles < x_high)
   last_index <- below_x_high[length(below_x_high)]
   qq_data <- data.frame(x = ref_quantiles[first_index:last_index], y = test_quantiles[first_index:last_index])
+
+  ref_color = "green"
+  test_color = "blue"
+  densities <- densities_plot(samples, 
+                              ref_distribution, ref_dfunc, 
+                              test_distribution, test_dfunc, 
+                              ref_color, test_color, "Dichtefunktionen") + theme
+  test_cdf <- simple_ggplot(test_data, "p", "x", test_color, "Testverteilung") + theme
+  ref_cdf <- simple_ggplot(ref_data, "x", "p", ref_color, "Referenzverteilung") + theme
   
-  test_plot <- create_plot(test_data, "p", "x", "blue", test_distribution)
   qq_plot <- create_plot(qq_data, NULL, NULL, "black", "QQ-Plot")
   qq_plot <- qq_plot + geom_abline(intercept = 0, slope = 1, color = "red", linetype = "dashed")
-  ref_plot <- create_plot(ref_data, "x", "p", "green", ref_distribution)
+  qq_plot <- qq_plot + theme
   
-  (test_plot | qq_plot) / (empty_plot() | ref_plot) +
-    plot_layout(heights = c(1, 1))
+  (test_cdf | qq_plot) / (densities | ref_cdf) +
+    plot_layout(heights = c(1, 1), guides = 'collect') 
 }
 
-illustrate_qqnorm <- function(test_distribution, test_dfunc, test_qfunc) {
-  x_low = -3
-  x_high = 3
-  ref_distribution = "Standardnormalverteilung"
-  ref_dfunc = pnorm
-  ref_qfunc = qnorm
-  illustrate_qqplot(x_low, x_high,
-                    ref_distribution, ref_dfunc, ref_qfunc,
-                    test_distribution, test_dfunc, test_qfunc)
+illustrate_qqnorm <- function(test_distribution, test_dfunc, test_pfunc, test_qfunc) {
+  illustrate_qqplot(-3, 3,
+                    "Standardnormalverteilung", dnorm, pnorm, qnorm, 
+                    test_distribution, test_dfunc, test_pfunc, test_qfunc,
+                    rstudio_theme())
 }
 
 qqnorm_t_distribution <- function(df) {
-  test_distribution = sprintf("t-Verteilung df=%d",df)
-  test_dfunc = partial_func(pt, df=df)
-  test_qfunc = partial_func(qt, df=df)
-  
-  illustrate_qqnorm(test_distribution, test_dfunc, test_qfunc)
+  illustrate_qqnorm(sprintf("t-Verteilung df=%d",df),
+                    partial_func(dt, df=df),
+                    partial_func(pt, df=df),
+                    partial_func(qt, df=df))
 }
 
 gg <- qqnorm_t_distribution(1)
 print(gg)
-
-qq_plot_t_distribution(1)
-qq_plot_t_distribution(10)
